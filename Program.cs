af4927177044f2236343e64d5a2bd25fc623cec7
halil <halil@DESKTOP-38P5TGT>
2022-04-25 05:29:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntegerCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 1000000; i++)
+            {
+                numList.Append(rnd.Next());
+            }
+            numList.Sort();
+
+
+
+
+        }
+    }
+}
